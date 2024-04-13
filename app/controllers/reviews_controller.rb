0218d7631ab36ac24027b76ca92a2d4c41183c02
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-13 12:33:22 -0500

    Refactored routes and Reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,7 +18,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
-    redirect_to restaurant_path, status: :see_other
+    redirect_to restaurant_path(@review.restaurant), status: :see_other
   end
 
   private
