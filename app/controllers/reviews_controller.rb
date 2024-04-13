b0ff6a24c86aa3445ac27b95321db32fa590ef99
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-13 12:01:20 -0500

    Reviews controller tests passed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,38 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[ new create ]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save ? redirect_to(restaurant_path(@restaurant)) : render("new")
+  end
+
+  def edit
+
+  end
+
+  def update
+    @review.update(review_params)
+    redirect_to review_path(@review)
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path, status: :see_other
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: %i[ new create ]
+  end
 
 end
